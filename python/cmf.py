eeb32e4fe1d424a110f10ced964e176bc679f728
Hoel Kervadec <hoel.kervadec.1@etsmtl.net>
2017-10-11 14:58:23 -0400

    Renamed some variables, λ, α

--- python/cmf.py
+++ python/cmf.py
@@ -19,12 +19,12 @@ def CMF_2D(ur, params):
     Ct = np.abs(ur - ulab[1])
 
     # set the initial values
-    # - the initial value of u is set to be an initial cut, see below.
+    # - the initial value of λ is set to be an initial cut, see below.
     # - the initial values of two terminal flows ps and pt are set to be the
     # specified legal flows.
     # - the initial value of the spatial flow fiels p = (pp1, pp2) is set to
     # be zero.
-    u = np.asarray((Cs - Ct) >= 0, np.float64)
+    λ = np.asarray((Cs - Ct) >= 0, np.float64)
     ps = np.minimum(Cs, Ct)  # Minimum element wise between the two
     pt = ps
 
@@ -38,12 +38,12 @@ def CMF_2D(ur, params):
         # update the spatial flow field p = (pp1, pp2):
         # the following steps are the gradient descent step with steps as the
         # step-size.
-        pts = divp - (ps - pt + u/cc)
+        pts = divp - (ps - pt + λ/cc)
         pp1[:, 1:-1] += steps * (pts[:, 1:] - pts[:, :-1])
         pp2[1:-1, :] += steps * (pts[1:, :] - pts[:-1, :])
 
         # the following steps give the projection to make |p(x)| <= α(x)
-        squares = pp1[:, :-1]**2  + pp1[:, 1:]**2 + pp2[:-1, :]**2 + pp2[1:, :]**2
+        squares = pp1[:, :-1]**2 + pp1[:, 1:]**2 + pp2[:-1, :]**2 + pp2[1:, :]**2
         gk = np.sqrt(squares * .5)
         gk = (gk <= α) + np.logical_not(gk <= α) * (gk / α)
         gk = 1 / gk
@@ -54,32 +54,32 @@ def CMF_2D(ur, params):
         divp = pp1[:, 1:] - pp1[:, :-1] + pp2[1:, :] - pp2[:-1, :]
 
         # updata the source flow ps
-        pts = divp + pt - u/cc + 1/cc
+        pts = divp + pt - λ/cc + 1/cc
         ps = np.minimum(pts, Cs)
 
         # update the sink flow pt
-        pts = - divp + ps + u/cc
+        pts = - divp + ps + λ/cc
         pt = np.minimum(pts, Ct)
 
-        erru = cc * (divp + pt - ps)
-        u -= erru
+        errλ = cc * (divp + pt - ps)
+        λ -= errλ
 
-        erriter[i] = np.sum(np.abs(erru)) / imgSize
+        erriter[i] = np.sum(np.abs(errλ)) / imgSize
 
         if erriter[i] < errbound:
-            return u, erriter, i
+            return λ, erriter, i
 
-    return u, erriter, numIter
+    return λ, erriter, numIter
 
 
-def plot(u, erriter):
+def plot(λ, erriter):
     fig, axes = plt.subplots(2)
 
-    axes[0].imshow(u)
-    axes[0].set_title("Segmentation result u")
+    axes[0].imshow(λ)
+    axes[0].set_title("Segmentation result λ")
 
     axes[1].plot(erriter)
-    axes[1].set_title("Erriter over i")
+    axes[1].set_title("abs(errλ) over i")
 
     plt.show()
 
@@ -88,6 +88,7 @@ if __name__ == "__main__":
     im_file = "../data/cameraman.jpg"
 
     ur = sp.misc.imread(im_file) / 255
+    plt.imshow(ur)
     rows, cols = ur.shape
 
     varParas = [rows, cols, 300, 1e-4, 0.3, 0.16]
@@ -99,9 +100,9 @@ if __name__ == "__main__":
     para 5 - the step-size for the graident-projection of p
     """
 
-    u, erriter, i = CMF_2D(ur, varParas)
+    λ, erriter, i = CMF_2D(ur, varParas)
     print("Iterations: {}".format(i))
     print("Erriter mean: {}".format(np.mean(erriter)))
-    print("U mean: {}".format(np.mean(u)))
+    print("λ mean: {}".format(np.mean(λ)))
 
-    plot(u, erriter[:i])
+    plot(λ, erriter[:i])
